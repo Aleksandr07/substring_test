161e0715dbc64c9d1d3f7228ca5720833bb56577
Aleksandr <a-katushnii@yandex.ru>
2024-01-14 21:10:56 +0300

    Выполненная задача по билету

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-
+        String str = "Hello world";
+        int x = 7;
+        String str1 = str.substring(0, x);
+        String str2 = str.substring(x);
+        System.out.println("str1 = " + str1);
+        System.out.println("str2 = " + str2);
     }
 }
